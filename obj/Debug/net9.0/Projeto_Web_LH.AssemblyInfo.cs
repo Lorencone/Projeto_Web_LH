74fdec60a51d17559e20cc613158729e1931fb3d
Lorencone <mmlorencone@gmail.com>
2025-11-21 18:20:31 -0300

    Codificação para o funcionamento
    Codificação para o acesso ao banco e as páginas index, lista.

--- obj/Debug/net9.0/Projeto_Web_LH.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto_Web_LH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Web_LH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1491c4f73b495dcb83ac8d4eb00972da6202c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Web_LH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Web_LH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
